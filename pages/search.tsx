73235b8cbb402785103d6de2d4c225d240c3d90d
Serge <sergesoroka@gmail.com>
2022-08-17 13:12:43 +0300

    Search Page Created

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,9 +1,13 @@
-import SearchAutoComplete from "../components/SearchAutoComplete/SearchAutoComplete"
-
+import SearchAutoComplete from "../components/SearchAutoComplete/SearchAutoComplete";
+import TagsLayout from "../components/Tag/TagsLayout";
+import styles from "./../styles/Search.module.scss";
 const search = () => {
   return (
-    <SearchAutoComplete />
-  )
-}
+    <div className={styles.searchWrap}>
+      <SearchAutoComplete />
+      <TagsLayout />
+    </div>
+  );
+};
 
-export default search
+export default search;
