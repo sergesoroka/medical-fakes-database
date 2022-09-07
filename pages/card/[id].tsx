2e061f8e4bfa715b6ba5fe445788ae222ef223d8
Serge <sergesoroka@gmail.com>
2022-09-07 14:43:32 +0300

    Added mocka text

--- pages/card/[id].tsx
+++ pages/card/[id].tsx
@@ -3,7 +3,7 @@ import styles from "./../../styles/Home.module.scss";
 import TagsLayout from "../../components/Tag/TagsLayout";
 
 import YouTubeVideo from "../../components/YouTubeVideo/YouTubeVideo";
-import Statistic from "../../components/Statistic/Statistic";
+import Statistic from '../../components/Statistic/Statistic'
 import VoxArticlesLayout from "../../components/VoxArticles/VoxArticlesLayout";
 import { FaHandPointUp, FaThumbsUp } from "react-icons/fa";
 import { sample } from "./../../data/sample";
@@ -18,40 +18,34 @@ function CartPage() {
         <>
           <div>
             <div className={styles.themeSection}>
-              <div className={styles.wordTheme}>ТЕМА</div>
-              <div className={styles.arrowTheme}></div>
+            <div className={styles.wordTheme}>ТЕМА</div>
+            <div className={styles.arrowTheme}></div>
               <h3 className={styles.theme}>{item.theme}</h3>
             </div>
             <h2 className={styles.subtheme}>{item.subtheme}</h2>
             <div style={{ marginLeft: "4rem" }}>
               {/* @ts-ignore */}
-              {item.tags && <TagsLayout tags={item.tags} />}
+             {item.tags && <TagsLayout tags={item.tags}/>}
             </div>
             <div className={styles.verdictWrap}>
               <FaHandPointUp style={{ color: "#D12020", fontSize: "1.4rem" }} />
               <h4 className={styles.verdict}>{item.verdict}</h4>
             </div>
-            <p
-              className={styles.textDiscript}
-              dangerouslySetInnerHTML={{ __html: item.discription }}
-            />
+            <p className={styles.textDiscript}  dangerouslySetInnerHTML={{__html: item.discription}}/>
             <div className={styles.verdictWrap}>
               <FaThumbsUp style={{ color: "#008A40", fontSize: "1.2rem" }} />
               <h4 className={styles.proof}>Як насправді?</h4>
             </div>
-            <p
-              className={styles.textDiscript}
-              dangerouslySetInnerHTML={{ __html: item.disproof }}
-            />
+            <p className={styles.textDiscript} dangerouslySetInnerHTML={{__html: item.disproof}}/>
+             
+            
 
             <div className={styles.articlesWrap}>
               {/* <YouTubeVideo /> */}
               <VoxArticlesLayout />
             </div>
           </div>
-          <div>
-            <Statistic />
-          </div>
+          <div><Statistic /></div>
         </>
       );
     }
