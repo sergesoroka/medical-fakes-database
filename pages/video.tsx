e5939525f2c764032a1583948a96af55385f827b
Serge <sergesoroka@gmail.com>
2022-09-08 15:16:42 +0300

    Added Video Page

--- pages/video.tsx
+++ pages/video.tsx
@@ -1,7 +1,10 @@
+import YouTubeVideo from "../components/YouTubeVideo/YouTubeVideo";
 import styles from "./../styles/Home.module.scss";
 
 const Video = () => {
-  return <div className={styles.singlePageMainContent}>Відео</div>;
+  return <div className={styles.singlePageMainContent}>
+    <YouTubeVideo />
+  </div>;
 };
 
 export default Video;
