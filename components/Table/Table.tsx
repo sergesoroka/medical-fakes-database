cef0cca96ee3ebe24622298160f4af23244d7a7c
Serge <sergesoroka@gmail.com>
2022-09-23 12:02:20 +0300

    About and Method Pages, fixed_

--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -1,73 +1,9 @@
-import React from "react";
+import React from 'react'
 
-
-
-const Table = () => {
-  const COLUMS = [
-    {
-      accessor: "id",
-      label: "ID",
-    },
-    {
-      accessor: "theme",
-      label: "Тема",
-    },
-    {
-      accessor: "subtheme",
-      label: "Підтема",
-    },
-    {
-      accessor: "discription",
-      label: "Опис підтеми",
-    },
-    {
-      accessor: "verdict",
-      label: "Вердикт",
-    },
-    {
-      accessor: "source",
-      label: "ЗМІ / Джерело",
-    },
-    {
-      accessor: "date",
-      label: "Дата публікації",
-    },
-    {
-      accessor: "archive_link",
-      label: "Архівоване посилання",
-    },
-    {
-      accessor: "country",
-      label: "Країна джерела",
-    },
-    {
-      accessor: "disproof",
-      label: "Спростування",
-    },
-  ];
+function Table() {
   return (
-    <div>
-    <table>
-      <thead>
-        {COLUMS.map((item) => (
-          <th key={item.accessor}>{item.label}</th>
-        ))}
-      </thead>
-      <tbody>{tableData.map((item, i) => <tr key={item.source}>
-        <td>{i + 1}</td>
-        <td>{item.theme}</td>
-        <td>{item.subtheme}</td>
-        <td>{item.discription}</td>
-        <td>{item.verdict}</td>
-        <td>{item.date}</td>
-        <td></td>
-        <td></td>
-        <td>{item.country}</td>
-        <td>{item.disproof}</td>
-        
-      </tr>)}</tbody>
-    </table></div>
-  );
-};
+    <div>Table</div>
+  )
+}
 
-export default Table;
+export default Table
